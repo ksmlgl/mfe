a17ddd9d80f6bfd6348731a2571c936f00ec51ee
ksmlgl <kasimaligul@gmail.com>
2021-06-03 22:55:44 +0300

    ds

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json')
@@ -20,10 +20,7 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared :packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
+        })
     ],
 };
 module.exports = merge(commonConfig, devConfig);
