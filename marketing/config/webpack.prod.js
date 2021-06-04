4642f6e2ce3390b5a37ee5154f4b983921adc3b0
ksmlgl <kasimaligul@gmail.com>
2021-06-05 00:11:26 +0300

    added marketing deployment

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -1,24 +1,25 @@
-const {merge } = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
 const prodConfig = {
     mode: 'production',
-    output:{
-        filename:'[name].[contenthash].js'
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
     },
-    plugins:[
+    plugins: [
         new ModuleFederationPlugin({
-            name:'marketing',
-            filename:'remoteEntry.js',
-            exposes:{
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
                 './MarketingApp': './src/bootstrap'
             },
-            shared:packageJson.dependencies
+            shared: packageJson.dependencies
 
         })
-        
+
     ]
 };
 
