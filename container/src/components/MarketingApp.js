144c966fe9d3718626109e8e82fa70990a2e64bd
ksmlgl <kasimaligul@gmail.com>
2021-06-08 22:48:34 +0300

    auth project

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 import { mount } from 'marketing/MarketingApp'
+import { useHistory } from 'react-router-dom'
 
-export default () =>{
+export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                if (history.location !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />;
 
