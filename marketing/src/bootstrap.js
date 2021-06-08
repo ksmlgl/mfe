ea6c8a1e4698bd008f9f152a7905c2caa37ee066
ksmlgl <kasimaligul@gmail.com>
2021-06-08 23:03:22 +0300

    fix

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
-const mount = (el, { onNavigate, defaultHistory }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
-    const history = defaultHistory || createMemoryHistory();
+    const history = defaultHistory || createMemoryHistory({
+        initialEntries: [initialPath]
+    });
 
     if (onNavigate) {
         history.listen(onNavigate);
