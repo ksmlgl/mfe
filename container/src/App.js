c3f7ed8310346898d6f8b7f3552f3b67c5a6a7c8
ksmlgl <kasimaligul@gmail.com>
2021-06-08 23:26:00 +0300

    progress bar

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import Progress from './components/Progress'
 import Header from './components/Header'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
@@ -19,7 +19,7 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <Suspense fallback={<div>Loading...</div>}>
+                    <Suspense fallback={<Progress />}>
                         <Switch>
                             <Route path="/auth" component={AuthLazy} />
                             <Route path="/" component={MarketingLazy} />
